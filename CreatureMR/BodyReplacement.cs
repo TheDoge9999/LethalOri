21d9a36ee71597f9fe7b54227ffcb1e031f67628
TheDoge9999 <82960440+TheDoge9999@users.noreply.github.com>
2023-12-29 14:39:59 -0600

    Add files via upload

---
+++ CreatureMR/BodyReplacement.cs
@@ -0,0 +1,53 @@
+﻿using ModelReplacement;
+using UnityEngine;
+
+namespace CreatureModelReplacement
+{
+
+    public class BodyReplacement : BodyReplacementBase
+    {
+
+        protected override GameObject LoadAssetsAndReturnModel()
+        {
+            //Replace with the Asset Name from your unity project 
+            string model_name = "orimodel";
+            GameObject model = Assets.MainAssetBundle.LoadAsset<GameObject>(model_name);
+
+            Material replacementMat;
+
+            switch (StartOfRound.Instance.unlockablesList.unlockables[controller.currentSuitID].unlockableName)
+            {
+
+                case "OriOrange":
+                    replacementMat = Assets.MainAssetBundle.LoadAsset<Material>("orange");
+                    break;
+                case "OriGreen":
+                    replacementMat = Assets.MainAssetBundle.LoadAsset<Material>("green");
+                    break;
+                case "OriYellow":
+                    replacementMat = Assets.MainAssetBundle.LoadAsset<Material>("yellow");
+                    break;
+                case "OriBlue":
+                    replacementMat = Assets.MainAssetBundle.LoadAsset<Material>("blue");
+                    break;
+                default:
+                    replacementMat = Assets.MainAssetBundle.LoadAsset<Material>("orange");
+                    break;
+            }
+
+            SkinnedMeshRenderer[] meshes = model.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+            Debug.Log("Looking for meshes...");
+            foreach (SkinnedMeshRenderer mesh in meshes)
+            {
+                Debug.Log("Found a mesh: " + mesh.name);
+                //mesh.materials[0].SetTexture("_BaseColorMap", replacementMat);
+                mesh.SetMaterial(replacementMat);
+            }
+
+            return model;
+        }
+
+
+    }
+}
---
+++ CreatureMR/Plugin.cs
@@ -0,0 +1,92 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using System.Reflection;
+using ModelReplacement;
+using BepInEx.Configuration;
+using System;
+
+//using System.Numerics;
+
+namespace CreatureModelReplacement
+{
+
+
+    [BepInPlugin("LethalOri", "Dogered", "1.0.0")]
+    [BepInDependency("meow.ModelReplacementAPI", BepInDependency.DependencyFlags.HardDependency)]
+
+    
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ConfigFile config;
+
+        // Universal config options  
+        public static ConfigEntry<bool> enableModelForAllSuits { get; private set; }
+        public static ConfigEntry<bool> enableModelAsDefault { get; private set; }
+        public static ConfigEntry<string> suitNamesToEnableModel { get; private set; }
+        
+
+        private static void InitConfig()
+        {
+            enableModelForAllSuits = config.Bind<bool>("Suits to Replace Settings", "Enable Model for all Suits", false, "Enable to replace every suit with Model. Set to false to specify suits");
+            enableModelAsDefault = config.Bind<bool>("Suits to Replace Settings", "Enable Model as default", false, "Enable to replace every suit that hasn't been otherwise registered with Model.");
+            suitNamesToEnableModel = config.Bind<string>("Suits to Replace Settings", "Suits to enable Model for", "Default,Orange suit,Green suit,Pajama suit,Hazard suit", "Enter a comma separated list of suit names.(Additionally, [Green suit,Pajama suit,Hazard suit])");
+            
+        }
+        private void Awake()
+        {
+            config = base.Config;
+            InitConfig();
+            Assets.PopulateAssets();
+
+            // Plugin startup logic
+
+            if (enableModelForAllSuits.Value)
+            {
+                ModelReplacementAPI.RegisterModelReplacementOverride(typeof(BodyReplacement));
+
+            }
+            
+            if (enableModelAsDefault.Value)
+            {
+                ModelReplacementAPI.RegisterModelReplacementDefault(typeof(BodyReplacement));
+
+            }
+            ModelReplacementAPI.RegisterSuitModelReplacement("OriOrange", typeof(BodyReplacement));
+            ModelReplacementAPI.RegisterSuitModelReplacement("OriBlue", typeof(BodyReplacement));
+            ModelReplacementAPI.RegisterSuitModelReplacement("OriYellow", typeof(BodyReplacement));
+            ModelReplacementAPI.RegisterSuitModelReplacement("OriGreen", typeof(BodyReplacement));
+            /*
+            var commaSepList = suitNamesToEnableModel.Value.Split(',');
+            foreach (var item in commaSepList)
+            {
+                ModelReplacementAPI.RegisterSuitModelReplacement(item, typeof(BodyReplacement));
+            }
+            */
+
+            Harmony harmony = new Harmony("orimodel");
+            harmony.PatchAll();
+            Logger.LogInfo($"Plugin {"CreaturelReplacement"} is loaded!");
+        }
+    }
+    public static class Assets
+    {
+        // Replace mbundle with the Asset Bundle Name from your unity project 
+        public static string mainAssetBundleName = "orimodel";
+        public static AssetBundle MainAssetBundle = null;
+
+        private static string GetAssemblyName() => Assembly.GetExecutingAssembly().FullName.Split(',')[0];
+        public static void PopulateAssets()
+        {
+            if (MainAssetBundle == null)
+            {
+                using (var assetStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(GetAssemblyName() + "." + mainAssetBundleName))
+                {
+                    MainAssetBundle = AssetBundle.LoadFromStream(assetStream);
+                }
+
+            }
+        }
+    }
+
+}
